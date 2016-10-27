28676af08936855a7f06033cf03969e5fe1d1eb5
araki <araki@qoncept.co.jp>
2016-10-27 12:41:23 +0900

    Set scale anchor center

--- ImageTrimmer/Components/ScalableImageView.swift
+++ ImageTrimmer/Components/ScalableImageView.swift
@@ -11,13 +11,14 @@ class ScalableImageView: NSImageView {
         
         let zoomRecog = NSMagnificationGestureRecognizer(target: self, action: #selector(onZoom))
         addGestureRecognizer(zoomRecog)
+        
     }
     
     func onZoom(_ recognizer: NSMagnificationGestureRecognizer) {
         let magnification = recognizer.magnification
         let scaleFactor = (magnification >= 0.0) ? (1.0 + magnification) : 1.0 / (1.0 - magnification)
         
-        let location = recognizer.location(in: self)
+        let location = CGPoint(x: self.bounds.width/2 ,y: self.bounds.height/2)
         let move = CGPoint(x: location.x * (scaleFactor-1), y: location.y * (scaleFactor-1))
         
         self.layer!.sublayerTransform *= CATransform3DMakeScale(scaleFactor, scaleFactor, 1)
